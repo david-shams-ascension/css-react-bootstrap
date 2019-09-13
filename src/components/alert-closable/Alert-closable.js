dc3ee4e94db85aa58266277c77d4a30fce8b76f0
David Shams <david.shams@ascension-external.com>
2019-09-13 16:29:28 -0500

    <> </> not to add anything to DOM

--- src/components/alert-closable/Alert-closable.js
+++ src/components/alert-closable/Alert-closable.js
@@ -1,13 +1,17 @@
 import React, {useState} from 'react';
 import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
 
 export default function AlertClosable() {
      const [show, setShow] = useState(true);
     return (
-        <div>
-            <Alert dismissible variant="success">
+        <>
+            <Alert show={show} dismissible variant="success">
                 <Alert.Heading>Heading</Alert.Heading>
-            </Alert>]
-        </div>
+                <div className="d-flex justify-content-end">
+                <Button onClick={() => setShow(false)} >Hide/Show</Button>
+                </div>
+            </Alert>
+        </>
     )
 }
